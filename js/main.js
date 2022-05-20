f18aafd1d73be63798ed9a86eed8a2f352658a1e
Juan252003 <jddiaz355@misena.edu.co>
2022-05-19 19:02:15 -0500

    Haciendo Reactivo el input de busqueda

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 const API = "https://api.github.com/users/";
 
-async function Buscar(){
-    const response = await fetch(API + 'Juan252003')
-    //ahora quiero traer la info en formato json
-    const data = await response.json()
-    console.log(data)
-}
+    //se cortola funcion y se pego en el methods
 
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Hola SENA!'
+            busqueda: null,
+            result: null
         }
-    }
+    },
+    methods: {
+
+        //la palabra funciont no es necesaria, porque se usa un metodo
+        async Buscar(){
+            const response = await fetch(API + this.busqueda)
+            //ahora quiero traer la info en formato json
+            const data = await response.json()
+            console.log(data)
+            this.result = true
+        }   
+    },
 }) //Montamos esta informacion en el div app
